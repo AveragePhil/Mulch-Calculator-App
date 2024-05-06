69e7ed8cd8269345354b6e4ec2ee0af81e83e1ed
Novotny_Filip <novof107@hsd.k12.or.us>
2024-05-06 14:27:57 -0700

    finishing functionality

--- mulch_calculator/controller.py
+++ mulch_calculator/controller.py
@@ -3,12 +3,13 @@
 
 # Functions
 def calculate_mulch(a: int, d: int) -> int:
-    mulch = 0
+    mulch = (a*100)*(d*0.1)
     return mulch
 
 def display_results(m: int) -> str:
-    output = f"The amount of mulch needed is {m} liters/square decimeters."
+    output = f"The amount of mulch needed is {m} liters/cubic decimeters."
     return output
+
 # Global Scope
 if __name__ == "__main__":
     print("Global scope")
